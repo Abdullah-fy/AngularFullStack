04f70c23b17dd9849c9422afcb0718899153b217
Fatma <fatmaelsheikh958@gmail.com>
2025-01-31 21:43:42 +0200

    Home Page without Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "./Component/Home/hero/hero.component";
+import { BestSellerComponent } from "./Component/Home/best-seller/best-seller.component";
+import { NewArrivalComponent } from "./Component/Home/new-arrival/new-arrival.component";
+import { CategoriesComponent } from "./Component/Home/categories/categories.component";
+import { FooterComponent } from "./core/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeroComponent, BestSellerComponent, NewArrivalComponent, CategoriesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
