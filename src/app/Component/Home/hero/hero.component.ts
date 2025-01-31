60be575c9abf4769f4dc537128cd592aae93a7bb
Fatma <fatmaelsheikh958@gmail.com>
2025-01-31 22:50:29 +0200

    home page

---
+++ src/app/Component/Home/hero/hero.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-hero',
+  imports: [FormsModule],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+  
+
+}
