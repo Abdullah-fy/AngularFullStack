ab91399e64863ea54980c7c25620fcb55ddf0ec0
Fatma <fatmaelsheikh958@gmail.com>
2025-02-03 00:14:51 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,11 @@ import { BestSellerComponent } from "./Component/Home/best-seller/best-seller.co
 import { NewArrivalComponent } from "./Component/Home/new-arrival/new-arrival.component";
 import { CategoriesComponent } from "./Component/Home/categories/categories.component";
 import { FooterComponent } from "./core/footer/footer.component";
+import { ProductComponent } from "./Component/product/product.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeroComponent, BestSellerComponent, NewArrivalComponent, CategoriesComponent, FooterComponent],
+  imports: [HeroComponent, BestSellerComponent, NewArrivalComponent, CategoriesComponent, FooterComponent, ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
